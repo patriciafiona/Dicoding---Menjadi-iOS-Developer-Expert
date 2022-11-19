e3f40376ef99425251b2064b706fcfcfb4376d4a
Patricia Fiona <patriciafiona3@gmail.com>
2022-11-19 07:55:03 +0700

    [Submission 01] Init iOS project
    Do;
    - Create project
    - Add Cocoapods library used later
    - Create clean architecture structure
    
    Next:
    - Migrate Home Page to the SwiftUI design (from previous Gametopia project in Fundamental iOS)

---
+++ Submission 01/Gametopia/Gametopia/App/GametopiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GametopiaApp.swift
+//  Gametopia
+//
+//  Created by Patricia Fiona on 19/11/22.
+//
+
+import SwiftUI
+
+@main
+struct GametopiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
